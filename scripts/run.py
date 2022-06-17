6dade980e1d794e899159f7e9b2f777a788b1a0f
Mamata-s <mamata.uark@gmail.com>
2022-06-16 19:52:46 -0500

    minor changes

--- scripts/run.py
+++ scripts/run.py
@@ -12,9 +12,15 @@ os.system("python generate_full.py --dataset_size='MRI_50um'")
 
 #Generate Patch Data
 # Generate train patch data from z-axis 50um images for factor 2
-os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=64 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-64/factor_2/train/' --output-label-dir='../resolution_dataset50/patch/patch-64/factor_2/label/train/'")
+os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_2/train/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_2/label/train/'")
 # # Generate val patch data from z-axis 50um images for factor 2
-os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=64 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-64/factor_2/val/' --output-label-dir='../resolution_dataset50/patch/patch-64/factor_2/label/val/'")
+os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-64/factor_2/val/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_2/label/val/'")
 
 
+
+#Generate Patch Data
+# Generate train patch data from z-axis 50um images for factor 4
+os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_4/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_4/train/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_4/label/train/'")
+# # Generate val patch data from z-axis 50um images for factor 4
+os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_4/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_4/val/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_4/label/val/'")
 
