5792bfb52abef34174b1a6580dd0684d54b1ed55
Mamata-s <mamata.uark@gmail.com>
2022-06-29 13:50:31 -0500

    test set added for resolution 25 z-axis images from crop kspace only

--- scripts/generate_z_axis.py
+++ scripts/generate_z_axis.py
@@ -1,3 +1,4 @@
+#added test set for z axis image for 25 micron only
 import utility as ut
 import argparse
 import os
@@ -7,7 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_size',type=str,required=False,default='MRI_25um')
     args = parser.parse_args()
 
-    factor = [2,4,6,8]
+    factor = [2,4,6]
 
     dataset_size = args.dataset_size
     
@@ -16,11 +17,20 @@ if __name__ == '__main__':
         start = 20
         end= 260
         num_img = end-start
+        # genrating the test set index list and val set index list 
+        test_list = [256, 129, 130, 133, 135, 136, 141, 147, 20, 150, 151, 152, 153, 154, 157, 29, 32, 33, 162, 164, 37, 165, 167, 168, 41, 42,
+        175, 176, 181, 53, 182, 55, 186, 61, 191, 192, 66, 70, 71, 200, 76, 205, 78, 207, 80, 212, 214, 88, 90, 219, 95, 224, 96, 223, 227, 228, 101, 102, 103,
+        231, 108, 109, 238, 112, 240, 115, 116, 247, 248, 122, 251, 124, 253]
     else:
        dataset_name = 'resolution_dataset50' 
        start = 42
        end = 112
        num_img = end-start
+       print('create a test_index_list for 50 micron dataset')
+
+
+    original_list = [x for x in range(20,260)]
+    val_list = list(set(original_list) - set(test_list))
 
     # # Z-axis Images
     # ### F1 160
@@ -31,6 +41,7 @@ if __name__ == '__main__':
     main_path= '../{}/z_axis/'.format(dataset_name)
     label_train_dir = '../{}/z_axis/label/train/'.format(dataset_name)
     label_val_dir ='../{}/z_axis/label/val/'.format(dataset_name)
+    label_test_dir ='../{}/z_axis/label/test/'.format(dataset_name)
     hr_path = 'hr_f1_160'
 
 
@@ -38,13 +49,18 @@ if __name__ == '__main__':
        os.makedirs(label_train_dir)
     if not os.path.exists(label_val_dir):
        os.makedirs(label_val_dir)
+    if not os.path.exists(label_test_dir):
+        os.makedirs(label_test_dir)
     for fac in factor:
         train_path = main_path+ "factor_{}/train/".format(fac)
         val_path = main_path + "factor_{}/val/".format(fac)
+        test_path = main_path + "factor_{}/test/".format(fac)
         if not os.path.exists(train_path):
             os.makedirs(train_path)
         if not os.path.exists(val_path):
             os.makedirs(val_path)
+        if not os.path.exists(test_path):
+            os.makedirs(test_path)
         
 
 
@@ -110,26 +126,42 @@ if __name__ == '__main__':
     main_path= '../{}/z_axis/'.format(dataset_name)
     hr_path = 'hr_f4_149'
 
+    # saving val set
     for fac in factor:
         x,y,z = data_hr.shape
-        k=start
-        for i in range(num_img):
-            img = ut.crop_pad_kspace(data_hr[:,:,k],pad=True,factor=fac)
+        for index in val_list:
+            img = ut.crop_pad_kspace(data_hr[:,:,index],pad=True,factor=fac)
             img = ut.normalize_image(img)
-            name = 'lr_f4_149_{}_z_{}'.format(fac,k)
-            k+=1
+            name = 'lr_f4_149_{}_z_{}'.format(fac,index)
             path = main_path+"factor_{}/val/".format(fac)
             ut.save_img_using_pil_lib(img,name,path)
 
     
-    # saving lr images
+    # saving hr images for val set
     x,y,z = data_hr.shape
-    k=start
-    for i in range(num_img):
-        name= hr_path+ '_z_'+str(k)
-        img = ut.normalize_image(data_hr[:,:,k])
+    for index in val_list:
+        name= hr_path+ '_z_'+str(index)
+        img = ut.normalize_image(data_hr[:,:,index])
         k+=1
         ut.save_img_using_pil_lib(img,name,label_val_dir)
+
+     # saving test set
+    for fac in factor:
+        x,y,z = data_hr.shape
+        for index in test_list:
+            img = ut.crop_pad_kspace(data_hr[:,:,index],pad=True,factor=fac)
+            img = ut.normalize_image(img)
+            name = 'lr_f4_149_{}_z_{}'.format(fac,index)
+            path = main_path+"factor_{}/test/".format(fac)
+            ut.save_img_using_pil_lib(img,name,path)
+
+    
+    # saving hr images for test set
+    x,y,z = data_hr.shape
+    for index in test_list:
+        name= hr_path+ '_z_'+str(index)
+        img = ut.normalize_image(data_hr[:,:,index])
+        ut.save_img_using_pil_lib(img,name,label_test_dir)
 
 
 
--- scripts/run.py
+++ scripts/run.py
@@ -2,7 +2,7 @@ import os
 
 # # Generate 25UM dataset
 
-# os.system("python generate_z_axis.py --dataset_size='MRI_25um'")
+os.system("python generate_z_axis.py --dataset_size='MRI_25um'")
 # os.system("python generate_full.py --dataset_size='MRI_25um'")
 
 # # # Generate 50UM dataset
@@ -10,19 +10,12 @@ import os
 # os.system("python generate_full.py --dataset_size='MRI_50um'")
 
 
-# #Generate Patch Data
 # # Generate train patch data from z-axis 50um images for factor 2
-# os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_2/train/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_2/label/train/'")
-# # # Generate val patch data from z-axis 50um images for factor 2
-# os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_2/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-64/factor_2/val/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_2/label/val/'")
-
-
-
-# #Generate Patch Data
-# # Generate train patch data from z-axis 50um images for factor 4
-# os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_4/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_4/train/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_4/label/train/'")
-# # # Generate val patch data from z-axis 50um images for factor 4
-# os.system("python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_4/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../resolution_dataset50/patch/patch-95/factor_4/val/' --output-label-dir='../resolution_dataset50/patch/patch-95/factor_4/label/val/'")
+# factor=[2,4]
+# for fac in factor:
+#     os.system(f"python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_{fac}/train'  --label-dir='../resolution_dataset50/z_axis/label/train' --patch-size=96 --stride=75 --output-image-dir='../resolution_dataset50/patch/patch-96/factor_{fac}/train/' --output-label-dir='../resolution_dataset50/patch/patch-96/label/train/'")
+#     # # Generate val patch data from z-axis 50um images for factor 2
+#     os.system(f"python create_patch.py --image-dir='../resolution_dataset50/z_axis/factor_{fac}/val'  --label-dir='../resolution_dataset50/z_axis/label/val' --patch-size=96 --stride=75 --output-image-dir='../resolution_dataset50/patch/patch-96/factor_{fac}/val/' --output-label-dir='../resolution_dataset50/patch/patch-96/label/val/'")
 
 # ***************************************************************************************************************************************************
 # GENERATE GAUSSIAN DATASET
@@ -57,25 +50,25 @@ import os
 # ***************************************************************************************************************************************************************
 # GENERATE BICUBIC DATASET
 
-os.system("python generate_z_axis_bicubic.py --dataset_size='MRI_25um'")
-os.system("python generate_full_bicubic.py --dataset_size='MRI_25um'")
+# os.system("python generate_z_axis_bicubic.py --dataset_size='MRI_25um'")
+# os.system("python generate_full_bicubic.py --dataset_size='MRI_25um'")
 
-# # Generate 50UM dataset
-os.system("python generate_z_axis_bicubic.py --dataset_size='MRI_50um'")
-os.system("python generate_full_bicubic.py --dataset_size='MRI_50um'")
-
-
-#Generate Patch Data
-# Generate train patch data from z-axis 50um images for factor 2
-os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_2/train'  --label-dir='../bicubic_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_2/train/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_2/label/train/'")
-# # Generate val patch data from z-axis 50um images for factor 2
-os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_2/val'  --label-dir='../bicubic_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../bicubic_dataset50/patch/patch-64/factor_2/val/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_2/label/val/'")
+# # # Generate 50UM dataset
+# os.system("python generate_z_axis_bicubic.py --dataset_size='MRI_50um'")
+# os.system("python generate_full_bicubic.py --dataset_size='MRI_50um'")
 
 
+# #Generate Patch Data
+# # Generate train patch data from z-axis 50um images for factor 2
+# os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_2/train'  --label-dir='../bicubic_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_2/train/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_2/label/train/'")
+# # # Generate val patch data from z-axis 50um images for factor 2
+# os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_2/val'  --label-dir='../bicubic_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../bicubic_dataset50/patch/patch-64/factor_2/val/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_2/label/val/'")
 
-#Generate Patch Data
-# Generate train patch data from z-axis 50um images for factor 4
-os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_4/train'  --label-dir='../bicubic_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_4/train/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_4/label/train/'")
-# # Generate val patch data from z-axis 50um images for factor 4
-os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_4/val'  --label-dir='../bicubic_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_4/val/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_4/label/val/'")
+
+
+# #Generate Patch Data
+# # Generate train patch data from z-axis 50um images for factor 4
+# os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_4/train'  --label-dir='../bicubic_dataset50/z_axis/label/train' --patch-size=95 --num-patch=15 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_4/train/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_4/label/train/'")
+# # # Generate val patch data from z-axis 50um images for factor 4
+# os.system("python create_patch.py --image-dir='../bicubic_dataset50/z_axis/factor_4/val'  --label-dir='../bicubic_dataset50/z_axis/label/val' --patch-size=95 --num-patch=10 --output-image-dir='../bicubic_dataset50/patch/patch-95/factor_4/val/' --output-label-dir='../bicubic_dataset50/patch/patch-95/factor_4/label/val/'")
 
